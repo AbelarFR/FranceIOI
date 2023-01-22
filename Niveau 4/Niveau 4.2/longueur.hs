2bfae882e10518159f064ff46ba1f541494b31cd
AbelarFR <123269319+AbelarFR@users.noreply.github.com>
2023-01-22 21:43:52 +0100

    Create longueur.hs

---
+++ Niveau 4/Niveau 4.2/longueur.hs
@@ -0,0 +1,29 @@
+--------------------------------------------------------------------------------
+-- Name:        Longueur des descriptions
+-- Purpose:     France-IOI niveau 4.2
+--
+-- Author:      Sébastien
+--
+-- Created:     22/01/2023
+--------------------------------------------------------------------------------
+
+import Control.Monad
+import Data.List
+import Data.Array
+
+main :: IO ()
+main = do
+    n <- readLn :: IO Int
+    let arbre0 = array (0, n) [(i, [] ) | i <- [0..n]]
+    liste <- fmap (zip [1..n] . map read . words) getLine :: IO [(Int, Int)]
+    let arbre = foldr (mettreDansBoite) arbre0 liste
+    print $ calculerProfondeur arbre (arbre!0)
+    
+mettreDansBoite :: (Int, Int) -> Array Int [Int] -> Array Int [Int]
+mettreDansBoite (contenu, i) arbre =
+    arbre // [(i, contenu:(arbre!i))]
+
+calculerProfondeur :: Array Int [Int] -> [Int] -> Int
+calculerProfondeur _ [] = 0
+calculerProfondeur arbre contenu =
+    (maximum (map (calculerProfondeur arbre . (!) arbre) contenu)) + 1
