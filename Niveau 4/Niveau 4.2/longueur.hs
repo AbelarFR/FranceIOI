b4ebe48532835dd10a257e3d3f17d1f53f43269d
AbelarFR <123269319+AbelarFR@users.noreply.github.com>
2023-01-23 22:22:10 +0100

    Optimisation en utilisant un mutable array

--- Niveau 4/Niveau 4.2/longueur.hs
+++ Niveau 4/Niveau 4.2/longueur.hs
@@ -7,24 +7,31 @@
 -- Created:     22/01/2023
 --------------------------------------------------------------------------------
 
-import Control.Monad
-import Data.List
 import Data.Array
+import Data.Array.IO
 import System.TimeIt
 
 main :: IO ()
 main = timeIt $ do
+    -- Nombre d'objets = nombre de noeuds de l'arbre
     n <- readLn :: IO Int
+    -- Liste de couples n° contenu (= fils), n° contenant (= père)
     liste <- fmap (zip [1..n] . map read . words) getLine :: IO [(Int, Int)]
-    let arbre0 = array (0, n) [(i, [] ) | i <- [0..n]]
-        arbre = foldr (mettreDansBoite) arbre0 liste
+    -- Crée l'arbre
+    arbreM <- newArray (0, n) [] :: IO (IOArray Int [Int])
+    sequence (map (initialiser arbreM) liste)
+    -- Transforme l'arbre en immutable array
+    arbre <- freeze arbreM :: IO (Array Int [Int])
     print $ calculerProfondeur arbre (arbre!0)
-    
-mettreDansBoite :: (Int, Int) -> Array Int [Int] -> Array Int [Int]
-mettreDansBoite (contenu, i) arbre =
-    arbre // [(i, contenu:(arbre!i))]
 
+-- Ajoute contenu dans la liste des fils du noeud n° i de l'arbre.    
+initialiser :: IOArray Int [Int] -> (Int, Int) -> IO ()
+initialiser arbre (contenu, i) = do
+    fils <- readArray arbre i
+    writeArray arbre i (contenu:fils)
+
+-- Calcule la profondeur sous un noeud avec l'ensemble de ces fils.
 calculerProfondeur :: Array Int [Int] -> [Int] -> Int
 calculerProfondeur _ [] = 0
-calculerProfondeur arbre contenu =
-    (maximum (map (calculerProfondeur arbre . (!) arbre) contenu)) + 1
+calculerProfondeur arbre fils =
+    (maximum (map (calculerProfondeur arbre . (!) arbre) fils)) + 1
